8708df2ffe2819ed02f1462b073420fc982d257f
Finfeny <va0622@edu.turku.fi>
2024-11-21 14:17:10 +0200

    login and out

--- index.php
+++ index.php
@@ -11,20 +11,24 @@ include 'dbyhteys.php';
     <title>TAItter</title>
 </head>
 <body>
-    <div class="login">             <!-- Kirjautuminen -->
-        <button class="loginButton" onclick="
-            document.querySelector('.loginButton').style.display = 'none';      // Piilotetaan login-nappi
-            document.querySelector('.loginForm').style.display = 'block';       // ja näytetään login-form
+    <div id="login">             <!-- Kirjautuminen -->
+        <button id="loginButton" onclick="
+            document.querySelector('#loginButton').style.display = 'none';      // Piilotetaan login-nappi
+            document.querySelector('#loginForm').style.display = 'block';       // ja näytetään login-form
             ">Login
         </button>
-        <form class="loginForm" action="login.php" method="POST" style="display: none">
+        <form id="loginForm" action="login.php" method="POST" style="display: none">
             <input id ="inputbox" type="text" name="name" id="name">
             <input id ="inputbox" type="password" name="password" id="password">
             <input id ="sendbutton" type="submit" value="Login">
         </form>
     </div>
+    <div id="logout">
+        <form action="logout.php" method="POST">
+            <input type="submit" value="Logout">
+        </form>
 
-    <div class="user">              <!-- Käyttäjän tiedot -->
+    <div id="user">              <!-- Käyttäjän tiedot -->
         <?php
             session_start();
             if (isset($_SESSION["user"])) {
@@ -33,7 +37,7 @@ include 'dbyhteys.php';
         ?>
     </div>
     
-    <div class="posts">             <!-- Haetaan viestit databasesta -->
+    <div id="posts">             <!-- Haetaan viestit databasesta -->
         <?php
             
         $posts = $conn->query("SELECT * FROM `posts`")->fetchAll();
@@ -42,8 +46,8 @@ include 'dbyhteys.php';
             echo "<div class='post'>";
             $user = $conn->query("SELECT * FROM `users` WHERE `id` = " . $post['sender'])->fetch();
             echo $user["name"] . "<br><div class='contentRow'>";
-            if ($post["sender"] == $_SESSION["user"]["id"]) {
-                ?>
+            if (isset($_SESSION["user"]["id"]) && $post["sender"] == $_SESSION["user"]["id"]) {
+                ?>                                                              <!-- Viestin muokkaus -->
                 <button class="editButton" onclick="
                     this.closest('.post').querySelector('.editButton').style.display = 'none';
                     this.closest('.post').querySelector('.postContent').style.display = 'none';
@@ -60,9 +64,9 @@ include 'dbyhteys.php';
                 </form>
                 <?php
             }
-            echo "<div class='postContent'>" . $post["content"] . "</div>";
-            if ($post["sender"] == $_SESSION["user"]["id"]) {
-                ?>
+            echo "<div class='postContent'>" . $post["content"] . "</div>";     // Viestin sisältö
+            if (isset($_SESSION["user"]["id"]) && $post["sender"] == $_SESSION["user"]["id"]) {
+                ?>                                                            <!-- Viestin poisto -->
                 <form class="postDelete" action="deletepost.php" method="POST">
                     <input type="hidden" name="id" value="<?php echo $post['id'] ?>">
                     <input type="submit" value="Delete">
@@ -74,13 +78,18 @@ include 'dbyhteys.php';
 
         ?>
     </div>                         <!-- Viestien lähettäminen -->
-    <div class="sendbox">
-        <form action="sendpost.php" method="POST">
-            <input type="hidden" name="sender" value="<?php echo $_SESSION["user"]["id"] ?>">
-            <input id ="inputbox" type="text" name="content" id="content">
-            <input id ="sendbutton" type="submit" value="Send">
-        </form>
-    </div>
-
+    <?php
+    if (isset($_SESSION["user"])) {
+        ?>
+        <div id="sendbox">
+            <form action="sendpost.php" method="POST">
+                <input type="hidden" name="sender" value="<?php echo $_SESSION["user"]["id"] ?>">
+                <input id ="inputbox" type="text" name="content" id="content">
+                <input id ="sendbutton" type="submit" value="Send">
+            </form>
+        </div>
+        <?php
+    }
+    ?>
 </body>
 </html>
