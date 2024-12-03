2d04a7809fbf2aeb6a8d87e4dacc07170f7ec3ee
Finfeny <va0622@edu.turku.fi>
2024-12-03 09:25:59 +0200

    tagin poisto melkee toiminnas

--- deletepost.php
+++ deletepost.php
@@ -5,13 +5,24 @@ $id = $_POST["id"];
 $hashtag_id = $conn->query("SELECT hashtag_id FROM post_hashtags WHERE post_id = $id");
 $hashtag_id = $hashtag_id->fetch(PDO::FETCH_ASSOC);
 var_dump($hashtag_id);
-var_dump("DELETE FROM `user_hashtags` WHERE `hashtag_id` = 12");
-$conn->prepare("DELETE FROM `user_hashtags` WHERE `hashtag_id` = :hashtag_id")->execute(["hashtag_id" => $hashtag_id]);
-// $conn->prepare("DELETE FROM `hashtags` WHERE `hashtag_id` = :hashtag_id")->execute(["hashtag_id" => $hashtag_id]);
-// $conn->prepare("DELETE FROM `post_hashtags` WHERE `post_id` = :id")->execute(["id" => $id]);
-// $conn->prepare("DELETE FROM `mentions` WHERE `post_id` = :id")->execute(["id" => $id]);
-// $conn->prepare("DELETE FROM `posts` WHERE `id` = :id")->execute(["id" => $id]);
+// Eka poistetaan käyttäjien seuraukset tagista
+$conn->
+prepare("DELETE FROM `user_hashtags` WHERE `hashtag_id` = :hashtag_id")->execute(["hashtag_id" => $hashtag_id["hashtag_id"]]);
 
-// header("Location: index.php");
+// Sitten poistetaan tagi postaustageista
+$conn->prepare("DELETE FROM `post_hashtags` WHERE `post_id` = :id")->execute(["id" => $id]);
+
+// Sitten poistetaan tagi tagitaulusta
+$conn->
+prepare("DELETE FROM `hashtags` WHERE `hashtag_id` = :hashtag_id")->execute(["hashtag_id" => $hashtag_id["hashtag_id"]]);
+
+// Sitten poistetaan maininnat
+$conn->
+prepare("DELETE FROM `mentions` WHERE `post_id` = :id")->execute(["id" => $id]);
+
+// Sitten poistetaan postaus
+$conn->prepare("DELETE FROM `posts` WHERE `id` = :id")->execute(["id" => $id]);
+
+header("Location: index.php");
 
 ?>
